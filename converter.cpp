b2f101a5da145b7e5d92b0f6fb81ebe623c2841b
nuraskiah <70048902+nuraskiah@users.noreply.github.com>
2020-08-23 00:00:00 +0800

    Update converter.cpp

--- converter.cpp
+++ converter.cpp
@@ -1,42 +1,33 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
- string t="_";
-
- void toCamelCase(string s){
-    string u;
-    while(s.find(t)!=string::npos){
-      u=toupper(s[s.find(t)+1]);
-      s.replace(s.find(t)+1, 1, u);
-      s.erase(s.find(t), 1);
-    }
- }
-
- void to_snake_case(string s){
-    string l;
-    while(s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ")!=string::npos){
-      l=tolower(s[s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ")]);
-      s.insert(s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), t);
-      s.replace(s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),1,l);
-    }
- 
- }
  
  int main(){
-    string s;
+    string s,u,b;
+    string t="_";
+    int a,c,i,j;
+    int arr[1000];
     cin>>s;
-
-    //check if string is camel or snake case
+ 
+    //to camel case
     if(s.find(t)!=string::npos){
-        toCamelCase(s);
-    } 
-    
-    else{
-        to_snake_case(s);
+        while(s.find(t)!=string::npos){
+            u=toupper(s[s.find(t)+1]);
+            s.replace(s.find(t)+1, 1, u);
+            s.erase(s.find(t), 1);
+        }
     }
-
+ 
+    //to snake case
+    else{
+        while(s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ")!=string::npos){
+            b=tolower(s[s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ")]);
+            s.insert(s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), t);
+            s.replace(s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),1,b);
+        }
+    }
+ 
     cout<<s<<endl;
-
+ 
     return 0;
  }
